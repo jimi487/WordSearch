04772e7bbb2c8b2c24282e1a9b27c1e7ff90f4da
Jimi <jimi487@gmail.com>
2020-01-20 22:06:05 -0500

    Game Fragment  - UI Updated slightly  - Table now loads in words from word bank
    TODO:
    Game Fragment
     - Implement Game logic
     - Implement UI Views for words at bottom
     - Implement submit button
     - Implement currently selected View

--- app/src/main/java/com/bohil/wordsearch/GameFragment.kt
+++ app/src/main/java/com/bohil/wordsearch/GameFragment.kt
@@ -6,27 +6,164 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TableRow
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.bohil.wordsearch.databinding.FragmentGameBinding
+import kotlin.random.Random
 
 
 class GameFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = GameFragment()
-    }
-
     private lateinit var viewModel: GameViewModel
+    private lateinit var binding: FragmentGameBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
-        // TODO: Use the ViewModel
+        val BOARD_SIZE = binding.gameRow0.virtualChildCount
+        viewModel.gameBoard = Array(BOARD_SIZE) { Array(BOARD_SIZE) { "-" } }
+
+
+        //Toast.makeText(activity, "character is: $BOARD_LENGTH", Toast.LENGTH_LONG).show()
+        // Game Board Initialization
+        setTags(BOARD_SIZE)
+        randomizeWords()
+        loadWords(BOARD_SIZE)
+        initializeTable(BOARD_SIZE)
+
+    }
+
+    /**
+     * Sets the tags for each button in the game board
+     */
+    private fun setTags(boardSize: Int) {
+        val tagString = "char_btn"
+        // Going through each table row
+        for (i in 0 until boardSize) {
+            val child = binding.gameGrid.getChildAt(i)
+            if (child is TableRow) {
+                val currentRow: TableRow = child
+                // Going through each table cell
+                for (j in 0 until boardSize) {
+                    currentRow.getChildAt(j).tag = tagString + i.toString() + j.toString()
+                }
+            }
+        }
+    }
+
+    private fun randomizeWords() {
+        viewModel.randomizeWords()
+    }
+
+    /**
+     * Loads the words into the game board
+     * Words in the word list are gone through in order
+     */
+    private fun loadWords(boardSize: Int = 10) {
+        var rowStart: Int
+        var colStart: Int
+        var orientation: Int
+        var spaceAvail: Boolean
+
+        for (word in viewModel.words) {
+            //Toast.makeText(activity, "Current word is $word", Toast.LENGTH_LONG).show()
+            // Deciding the word orientation
+            // Horizontal: 0    Vertical: 1
+            orientation = Random.nextInt(2)
+            // Finding a position on the table for the word
+            when (orientation) {
+                0 -> {
+                    do {
+                        spaceAvail = true
+                        colStart = if (word.length == boardSize) 0 else Random.nextInt(boardSize)
+                        rowStart = Random.nextInt(boardSize)
+
+                        // If the size of the word exceeds array bounds, use board size instead
+                        for (char in colStart until (if (colStart + word.length < boardSize) colStart + word.length else boardSize)) {
+                            if (viewModel.gameBoard[rowStart][char] != "-") {
+                                spaceAvail = false
+                            }
+                        }
+                    } while (word.length + colStart > boardSize || !spaceAvail)
+
+                    // Placing word into table
+                    var x = 0
+                    while (x < word.length) {
+                        viewModel.gameBoard[rowStart][colStart] = word[x].toString()
+                        x++
+                        colStart++
+                    }
+                }
+
+                1 -> {
+                    do {
+                        spaceAvail = true
+                        colStart = Random.nextInt(boardSize)
+                        rowStart = if (word.length == boardSize) 0 else Random.nextInt(boardSize)
+
+                        for (char in rowStart until (if (rowStart + word.length < boardSize) rowStart + word.length else boardSize)) {
+                            if (viewModel.gameBoard[char][colStart] != "-") {
+                                spaceAvail = false
+                            }
+                        }
+                    } while (word.length + rowStart > boardSize || !spaceAvail)
+
+                    // Placing word into table
+                    var x = 0
+                    while (x < word.length) {
+                        viewModel.gameBoard[rowStart][colStart] = word[x].toString()
+                        x++
+                        rowStart++
+                    }
+                }
+            }
+        }
+        binding.invalidateAll()
+    }
+
+    /**
+     * Refreshes the game board to display the updated character table
+     */
+    private fun initializeTable(boardSize: Int) {
+        // Rest of table filled with random characters
+        val ALLOWED_CHARACTERS = "abcdefghijklmnopqrstuvwxyz"
+        for (row in 0 until boardSize)
+            for (col in 0 until boardSize) {
+                if (viewModel.gameBoard[row][col] == "-")
+                    viewModel.gameBoard[row][col] =
+                        ALLOWED_CHARACTERS[Random.nextInt(ALLOWED_CHARACTERS.length)].toString()
+            }
+
+        for (i in 0 until boardSize) {
+            val child = binding.gameGrid.getChildAt(i)
+            if (child is TableRow) {
+                val currentRow: TableRow = child
+                for (j in 0 until boardSize) {
+                    val character = currentRow.getChildAt(j)
+                    if (character is Button) {
+                        val characterButton: Button = character
+                        characterButton.text = viewModel.gameBoard[i][j]
+                    }
+                }
+            }
+        }
+        binding.invalidateAll()
+    }
+
+    companion object {
+        fun newInstance() = GameFragment()
     }
 
 }
--- app/src/main/java/com/bohil/wordsearch/GameViewModel.kt
+++ app/src/main/java/com/bohil/wordsearch/GameViewModel.kt
@@ -1,7 +1,33 @@
 package com.bohil.wordsearch
 
 import androidx.lifecycle.ViewModel
+import kotlin.random.Random
 
 class GameViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    // Word bank
+    var words = arrayOf(
+        "swift", "kotlin", "objectivec",
+        "variable", "java", "mobile", "intern", "summer"
+    )
+
+    var gameBoard = arrayOf<Array<String>>()
+
+    /**
+     * Creates the letters inside of the table
+     * First shuffles the words inside the word array
+     * Then words are picked from the array and broken down into letters
+     * After a random spot has been picked that has enough space for all the letters, the letters
+     * are placed
+     * Continues until there are no more words inside the word bank
+     */
+    fun randomizeWords() {
+        for (i in words.indices) {
+            val randIntToSwap = Random.nextInt(words.size)
+            val temp = words[randIntToSwap]
+            words[randIntToSwap] = words[i]
+            words[i] = temp
+        }
+
+    }
+
 }
