1dda45b0474af77ed811d2855cffa0e370d49a38
Jimi <jimi487@gmail.com>
2020-01-23 12:50:01 -0500

    Workflow Optimization  - Added TODO's in area's that haven't been completed yet

--- app/src/main/java/com/bohil/wordsearch/GameFragment.kt
+++ app/src/main/java/com/bohil/wordsearch/GameFragment.kt
@@ -1,5 +1,7 @@
 package com.bohil.wordsearch
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,12 +11,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TableRow
 import android.widget.TextView
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.bohil.wordsearch.databinding.FragmentGameBinding
-import java.util.Timer
-import kotlin.concurrent.schedule
-import kotlin.random.Random
 
 
 class GameFragment : Fragment() {
@@ -30,160 +28,44 @@ class GameFragment : Fragment() {
 
         //Toast.makeText(activity, "character is: $BOARD_LENGTH", Toast.LENGTH_LONG).show()
         // Game Board Initialization
-        randomizeWords()
         loadWords()
+        fillTable()
 
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+        binding.gameViewModel = viewModel
         binding.lifecycleOwner = this
-        initializeTable()
+
+        refreshTable()
         initializeWordHints()
 
         return binding.root
     }
 
     /**
-     * Randomizes the words in the word list
-     */
-    private fun randomizeWords() {
-        viewModel.randomizeWords()
-    }
-
-    /**
      * Loads the words into the game board
      * Words in the word list are gone through in order
      */
-    private fun loadWords(boardSize: Int = 10) {
-        var rowStart = 0
-        var colStart = 0
-        var orientation: Int
-        var spaceAvail: Boolean
-        var exceedsBorder = false
+    private fun loadWords() {
+        viewModel.loadWords()
+    }
 
-        for (word in viewModel.words) {
-            //Toast.makeText(activity, "Current word is $word", Toast.LENGTH_LONG).show()
-            // Deciding the word orientation
-            // Horizontal: 0    Vertical: 1
-            orientation = Random.nextInt(2)
-            spaceAvail = false
-            // Finding a position on the board for the word
-            when (orientation) {
-                0 -> {
-                    while (!spaceAvail) {
-                        spaceAvail = true
-
-                        // Ensuring the word is within table borders
-                        do {
-                            colStart =
-                                if (word.length == boardSize) 0 else Random.nextInt(boardSize)
-                            rowStart = Random.nextInt(boardSize)
-                        } while (word.length + colStart > boardSize)
-
-                        // Ensuring there are no other characters in the spot for the word
-                        for (char in colStart until (if (rowStart + word.length < boardSize) colStart + word.length else boardSize)) {
-                            if (viewModel.gameBoard[rowStart][char] != "-") {
-                                spaceAvail = false
-                            }
-                        }
-                    }
-                    // Placing the word
-                        var x = 0
-                        while (x < word.length) {
-                            viewModel.gameBoard[rowStart][colStart] = word[x].toString()
-                            x++
-                            colStart++
-                        }
-
-
-/*                    do {
-                        spaceAvail = true
-                        colStart = if (word.length == boardSize) 0 else Random.nextInt(boardSize)
-                        rowStart = Random.nextInt(boardSize)
-
-                        // If the position of the word exceeds array bounds, use board size instead
-                        for (char in colStart until (if (colStart + word.length < boardSize) colStart + word.length else boardSize)) {
-                            if (viewModel.gameBoard[rowStart][char] != "-") {
-                                spaceAvail = false
-                            }
-                        }
-                    } while (word.length + colStart > boardSize || !spaceAvail)
-
-                    // Placing word into game board
-                    var x = 0
-                    while (x < word.length) {
-                        viewModel.gameBoard[rowStart][colStart] = word[x].toString()
-                        x++
-                        colStart++
-                    }*/
-                }
-
-                1 -> {
-                    while (!spaceAvail) {
-                        spaceAvail = true
-
-                        // Ensuring the word is within table borders
-                        do {
-                            colStart = Random.nextInt(boardSize)
-                            rowStart =
-                                if (word.length == boardSize) 0 else Random.nextInt(boardSize)
-                        } while (word.length + rowStart > boardSize)
-
-                        for (char in rowStart until (if (rowStart + word.length < boardSize) rowStart + word.length else boardSize)) {
-                            if (viewModel.gameBoard[char][colStart] != "-") {
-                                spaceAvail = false
-                            }
-                        }
-                    }
-
-                        var x = 0
-                        while (x < word.length) {
-                            viewModel.gameBoard[rowStart][colStart] = word[x].toString()
-                            x++
-                            rowStart++
-                        }
-
-
-                    /*
-                                       do {
-                                           spaceAvail = true
-                                           colStart = Random.nextInt(boardSize)
-                                           rowStart = if (word.length == boardSize) 0 else Random.nextInt(boardSize)
-
-                                           for (char in rowStart until (if (rowStart + word.length < boardSize) rowStart + word.length else boardSize)) {
-                                               if (viewModel.gameBoard[char][colStart] != "-") {
-                                                   spaceAvail = false
-                                               }
-                                           }
-                                       } while (word.length + rowStart > boardSize || !spaceAvail)
-
-                                       var x = 0
-                                       while (x < word.length) {
-                                           viewModel.gameBoard[rowStart][colStart] = word[x].toString()
-                                           x++
-                                           rowStart++
-                                       }*/
-                }
-            }
-        }
+    private fun fillTable() {
+        viewModel.fillTable()
     }
 
     /**
-     * Refreshes the game board to display the updated character table
+     * Refreshes game board to display the updated character table
      */
-    private fun initializeTable(boardSize: Int = 10) {
-        // Rest of table filled with random characters
-        val ALLOWED_CHARACTERS = "abcdefghijklmnopqrstuvwxyz"
-        for (row in 0 until boardSize)
-            for (col in 0 until boardSize) {
-                if (viewModel.gameBoard[row][col] == "-")
-                    viewModel.gameBoard[row][col] =
-                        ALLOWED_CHARACTERS[Random.nextInt(ALLOWED_CHARACTERS.length)].toString()
-            }
 
+    // TODO Implement case when its a new table. Currently colors do not change properly if theres a n
+    // new game
+    private fun refreshTable(boardSize: Int = 10) {
         // Setting the text for each button
         for (i in 0 until boardSize) {
             val child = binding.gameGrid.getChildAt(i)
@@ -192,8 +74,11 @@ class GameFragment : Fragment() {
                 for (j in 0 until boardSize) {
                     val character = currentRow.getChildAt(j)
                     if (character is Button) {
-                        val characterButton: Button = character
-                        characterButton.text = viewModel.gameBoard[i][j]
+                        val charBtn: Button = character
+                        charBtn.text = viewModel.gameBoard[i][j]
+                        // Adding the on clicks for each button
+                        charBtnClick(charBtn)
+
                     }
                 }
             }
@@ -207,7 +92,7 @@ class GameFragment : Fragment() {
         var i = 0
         var currentIndex = 0
 
-        // TODO: Handle case when amount of words in the word list is different than number of cells
+        // TODO: Handle case when amount of words in word list is different than amount in text views
         while (i < listSize) {
             val child = binding.wordHintTable.getChildAt(i)
             var j = 0
@@ -225,6 +110,26 @@ class GameFragment : Fragment() {
             }
             i++
         }
+    }
+
+    /**
+     * On Click logic for each character button
+     */
+
+    // TODO Fix how colors change when instance of new game
+    private fun charBtnClick(btn: Button) {
+        btn.setOnClickListener {
+            // Changing button color
+            val newColor = Color.argb(100, 145, 250, 145)
+            btn.setBackgroundColor(
+                if ((btn.background as ColorDrawable).color == Color.WHITE)
+                    newColor else Color.WHITE
+            )
+
+            // adding the text to the user's current selection
+            viewModel.addToSelection(btn.text.toString())
+        }
+
     }
 
     companion object {
